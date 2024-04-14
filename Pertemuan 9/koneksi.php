b1678a600611b3255535629bac2bfec3e6a5d0be
Mangsen <priyatmojoew@gmail.com>
2024-04-14 18:30:47 +0700

    Pertemuan 9 - Praktikum 2

---
+++ Pertemuan 9/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $connect = mysqli_connect("localhost", "root", "", "prakwebdb");
+
+    if (mysqli_connect_error()) {
+        die("Koneksi database gagal: " . mysqli_connect_error());
+    }
+?>
