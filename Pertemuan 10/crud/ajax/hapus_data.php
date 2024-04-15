308efd2cfd796dc12af1dcf770c205da2ce053ad
Mangsen <priyatmojoew@gmail.com>
2024-04-15 11:26:39 +0700

    Pertemuan 10 - PRaktikum 6

---
+++ Pertemuan 10/crud/ajax/hapus_data.php
@@ -0,0 +1,25 @@
+<?php
+// Memulai sesi PHP
+session_start();
+
+// Mengimpor koneksi database dan skrip CSRF protection
+include 'koneksi.php'; 
+include 'csrf.php';
+
+// Mengambil ID dari data yang akan dihapus dari database
+$id = $_POST['id'];
+
+// Query untuk menghapus data anggota berdasarkan ID
+$query = "DELETE FROM anggota WHERE id=?"; 
+
+// Persiapan query
+$sql = $db1->prepare($query);
+$sql->bind_param("i", $id); 
+$sql->execute();
+
+// Mengembalikan respons sukses dalam format JSON
+echo json_encode(['success' => 'Sukses']);
+
+// Menutup koneksi database
+$db1->close();
+?>
