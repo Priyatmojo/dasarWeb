561925a42576010bb751b73dde2208a1b8a8b4da
Mangsen <priyatmojoew@gmail.com>
2024-04-22 10:38:19 +0700

    Pertemuan 11 - Praktikum 1

--- Pertemuan 11/logout.php
+++ Pertemuan 11/logout.php
@@ -1,11 +1,7 @@
 <?php
-// Memeriksa apakah sesi sudah dimulai, jika belum, mulai sesi
-if(session_status() === PHP_SESSION_NONE)
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
-
-// Menghancurkan semua data sesi yang ada
+}
 session_destroy();
 
-// Mengarahkan pengguna kembali ke halaman utama (index.php)
-header('location:index.php');
-?>
+header("location:index.php");
